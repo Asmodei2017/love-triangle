60e5a3e48c1dbdf3670f17fe46aa81f558a123ed
Asmodei2017 <p.zabavskiy@tut.by>
2019-03-18 20:25:37 +0200

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let result = 0;
+  preferences.unshift(0);
+    
+  for(let lover = 1; lover < preferences.length; lover++){
+      let secondLover = preferences[lover];
+      let thirdLover = preferences[secondLover];               
+      result += lover == preferences[thirdLover];
+  }
+  return ((result - result % 3) / 3);
+}
+ 
+
